cd82476bf5057fd08e7d4c126a24fa263a582039
Quaydrion Butler <99278263+quaydrionb@users.noreply.github.com>
2024-01-26 17:03:41 -0500

    updates customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,29 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import InvoicesTable from '@/app/ui/invoices/table'; // Assuming you have an InvoicesTable component
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customer',
+};
+
 export default function Page() {
-    return <p>Customers Page</p>;
-  }
+  const query = '';
+  const currentPage = 1;
+  const totalPages = 10;
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<TableRowSkeleton />}>
+        <InvoicesTable query={query} currentPage={currentPage} />
+      </Suspense>
+
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
